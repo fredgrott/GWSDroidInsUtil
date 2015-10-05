45247b6558a2f3c7a29c898f01b7faafd41160bf
Fred Grott <fred.grott@gmail.com>
2015-10-05 00:02:45 -0500

    added library code

---
+++ library/src/main/java/com/github/shareme/gwsdroidinsutil/library/SystemAnimations.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2015 Fred Grott(aka shareme GrottWorkShop)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http:;//www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under License.
+ */
+package com.github.shareme.gwsdroidinsutil.library;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+/**
+ * SystemAnimations to enable turning off, in ApplicationTest:
+ * <code>
+ *
+ *    protected void setUp() throws Exception {
+ *                 super.setUp();
+ *                  systemAnimations = new SystemAnimations(getInstrumentation().getContext());
+ *                  systemAnimations.disableAll();
+ *
+ *    }
+ *
+ *
+ *    protected void tearDown() throws Exception {
+ *             super.tearDown();
+ *             systemAnimations.enableAll();
+ *    }
+ * </code>
+ *
+ *
+ * Created by fgrott on 10/4/2015.
+ */
+@SuppressWarnings("unused")
+public class SystemAnimations {
+
+    private static final String ANIMATION_PERMISSION = "android.permission.SET_ANIMATION_SCALE";
+    private static final float DISABLED = 0.0f;
+    private static final float DEFAULT = 1.0f;
+
+    private final Context context;
+
+    SystemAnimations(Context context) {
+        this.context = context;
+    }
+
+    void disableAll() {
+        int permStatus = context.checkCallingOrSelfPermission(ANIMATION_PERMISSION);
+        if (permStatus == PackageManager.PERMISSION_GRANTED) {
+            setSystemAnimationsScale(DISABLED);
+        }
+    }
+
+    void enableAll() {
+        int permStatus = context.checkCallingOrSelfPermission(ANIMATION_PERMISSION);
+        if (permStatus == PackageManager.PERMISSION_GRANTED) {
+            setSystemAnimationsScale(DEFAULT);
+        }
+    }
+
+    private void setSystemAnimationsScale(float animationScale) {
+        try {
+            Class<?> windowManagerStubClazz = Class.forName("android.view.IWindowManager$Stub");
+            Method asInterface = windowManagerStubClazz.getDeclaredMethod("asInterface", IBinder.class);
+            Class<?> serviceManagerClazz = Class.forName("android.os.ServiceManager");
+            Method getService = serviceManagerClazz.getDeclaredMethod("getService", String.class);
+            Class<?> windowManagerClazz = Class.forName("android.view.IWindowManager");
+            Method setAnimationScales = windowManagerClazz.getDeclaredMethod("setAnimationScales", float[].class);
+            Method getAnimationScales = windowManagerClazz.getDeclaredMethod("getAnimationScales");
+
+            IBinder windowManagerBinder = (IBinder) getService.invoke(null, "window");
+            Object windowManagerObj = asInterface.invoke(null, windowManagerBinder);
+            float[] currentScales = (float[]) getAnimationScales.invoke(windowManagerObj);
+            for (int i = 0; i < currentScales.length; i++) {
+                currentScales[i] = animationScale;
+            }
+            setAnimationScales.invoke(windowManagerObj, new Object[]{currentScales});
+        } catch (Exception e) {
+            Log.e("SystemAnimations", "Could not change animation scale to " + animationScale + " :'(");
+        }
+    }
+}
